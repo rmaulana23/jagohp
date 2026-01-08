cd78ef0c458ee83c509249ab04c89544952bfabb
rmaulana23 <rifki.mau@gmail.com>
2026-01-08 21:24:36 +0700

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -93,7 +93,7 @@ const Header: React.FC<HeaderProps> = ({ page, setPage, onLogoClick, isAdminAuth
                 rel="noopener noreferrer"
                 className="hidden md:flex items-center gap-2 px-5 py-2 rounded-xl text-sm bg-white text-[color:var(--accent1)] font-bold hover:bg-slate-100 transition-all shadow-md active:scale-95"
             >
-                <span>JAGO-HP App</span>
+                <span>↓ JAGO-HP App</span>
                 <AndroidIcon className="w-4 h-4 text-[#3DDC84]" />
             </a>
         )}
